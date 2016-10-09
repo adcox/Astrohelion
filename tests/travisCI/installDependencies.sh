d68efa26b9bbb457c147662b9ef0deba84b9dff3
Andrew Cox <andrewdcox10@gmail.com>
2016-10-09 17:32:17 -0400

    updated installation script again to avoid linking errors

--- tests/travisCI/installDependencies.sh
+++ tests/travisCI/installDependencies.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
 LOG="installLog.log"
+
 # Install Dependencies for Travis CI system
-mkdir deps
+mkdir -p deps
 cd deps
 
 sudo apt-get -qq update
 
 # Boost
 wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.gz
-tar -xvzf boost_1_62_0.tar.gz >> $LOG 2>&1
+tar -xvzf boost_1_62_0.tar.gz >> $LOG
 cd boost_1_62_0
 echo "Installing BOOST"
-./bootstrap.sh >> $LOG 2>&1
-sudo ./b2 -d0 install --with-filesystem --with-system --with-test >> $LOG 2>&1
+./bootstrap.sh >> $LOG
+sudo ./b2 -d0 install --with-filesystem --with-system --with-test >> $LOG 
 cd ..
 
 # CSpice
 wget http://naif.jpl.nasa.gov/pub/naif/toolkit//C/PC_Linux_GCC_64bit/packages/cspice.tar.Z
-tar -xvzf cspice.tar.Z >> $LOG 2>&1
+tar -xvzf cspice.tar.Z >> $LOG
 cd cspice
 echo "Installing CSPICE"
 sudo mkdir /usr/local/include/cspice
@@ -31,20 +32,27 @@ cd ..
 
 # MatIO
 wget https://sourceforge.net/projects/matio/files/matio/1.5.3/matio-1.5.3.tar.gz
-tar -xvzf matio-1.5.3.tar.gz >> $LOG 2>&1
+tar -xvzf matio-1.5.3.tar.gz >> $LOG
 cd matio-1.5.3
 echo "Installing MATIO"
-./configure >> $LOG 2>&1
-make >> $LOG 2>&1
-sudo make install >> $LOG 2>&1
+./configure >> $LOG
+make >> $LOG
+sudo make install >> $LOG
 cd ..
 
 # GSL
-wget http://mirror.nexcess.net/gnu/gsl/gsl-2.1.tar.gz
-tar -xvzf gsl-2.1.tar.gz >> $LOG 2>&1
-cd gsl-2.1
+wget http://mirror.nexcess.net/gnu/gsl/gsl-2.2.1.tar.gz
+tar -xvzf gsl-2.2.1.tar.gz >> $LOG
+cd gsl-2.2.1
 echo "Installing GSL"
-./configure >> $LOG 2>&1
-make >> $LOG 2>&1
-sudo make install >> $LOG 2>&1
+./configure >> $LOG
+make >> $LOG
+sudo make install >> $LOG
 cd ..
+
+echo "Adding /usr/local/lib to LD_LIBRARY_PATH"
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+LD_RUN_PATH=$LD_RUN_PATH:/usr/local/lib
+
+# Update the dynamic linker
+sudo ldconfig

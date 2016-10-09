a2834bb8e0e01f2a42231933b0cb02ec7f700e47
Andrew Cox <andrewdcox10@gmail.com>
2016-10-08 22:18:39 -0400

    fixed another path issue

--- tests/travisCI/installDependencies.sh
+++ tests/travisCI/installDependencies.sh
@@ -6,11 +6,10 @@ mkdir deps
 cd deps
 
 sudo apt-get -qq update
-sudo apt-get install gsl matio
 
 # Boost
 wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.gz
-tar -xvzf boost_1_62_0.tar.gz
+tar -xvzf boost_1_62_0.tar.gz >> $LOG
 cd boost_1_62_0
 echo "Installing BOOST"
 ./boostrap.sh >> $LOG
@@ -19,7 +18,7 @@ cd ..
 
 # CSpice
 wget http://naif.jpl.nasa.gov/pub/naif/toolkit//C/PC_Linux_GCC_64bit/packages/cspice.tar.Z
-tar -xvzf cspice.tar.Z
+tar -xvzf cspice.tar.Z >> $LOG
 cd cspice
 echo "Installing CSPICE"
 sudo cp -R include/* /usr/local/include/cspice/
@@ -30,7 +29,7 @@ cd ..
 
 # MatIO
 wget https://sourceforge.net/projects/matio/files/matio/1.5.3/matio-1.5.3.tar.gz
-tar -xvzf matio-1.5.3.tar.gz
+tar -xvzf matio-1.5.3.tar.gz >> $LOG
 cd matio-1.5.3
 echo "Installing MATIO"
 ./configure >> $LOG
@@ -40,7 +39,7 @@ cd ..
 
 # GSL
 wget http://mirror.nexcess.net/gnu/gsl/gsl-2.1.tar.gz
-tar -xvzf gsl-2.1.tar.gz
+tar -xvzf gsl-2.1.tar.gz >> $LOG
 cd gsl-2.1
 echo "Installing GSL"
 ./configure >> $LOG

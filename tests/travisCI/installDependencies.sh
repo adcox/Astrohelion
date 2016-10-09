51ebc6670da519c1018c8e62b9c6879b00e56780
Andrew Cox <andrewdcox10@gmail.com>
2016-10-08 22:06:03 -0400

    Updated install script to pipe output to a file instead of standard output

--- tests/travisCI/installDependencies.sh
+++ tests/travisCI/installDependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+LOG="installLog.log"
 # Install Dependencies for Travis CI system
 mkdir deps
 cd deps
@@ -10,13 +11,17 @@ sudo apt-get install gsl matio
 # Boost
 wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.gz
 tar -xvzf boost_1_62_0.tar.gz
-cd boost_1_62_0 && ./boostrap.sh && sudo ./b2 install
+cd boost_1_62_0
+echo "Installing BOOST"
+./boostrap.sh >> $LOG
+sudo ./b2 install >> $LOG
 cd ..
 
 # CSpice
 wget http://naif.jpl.nasa.gov/pub/naif/toolkit//C/PC_Linux_GCC_64bit/packages/cspice.tar.Z
 tar -xvzf cspice.tar.Z
 cd cspice
+echo "Installing CSPICE"
 sudo cp -R include/* /usr/local/include/cspice/
 sudo mv lib/cspice.a /usr/local/lib/libcspice.a
 sudo mv lib/csupport.a /usr/local/lib/libcsupport.a
@@ -27,14 +32,20 @@ cd ..
 wget https://sourceforge.net/projects/matio/files/matio/1.5.3/matio-1.5.3.tar.gz
 tar -xvzf matio-1.5.3.tar.gz
 cd matio-1.5.3
-./configure && make && sudo make install
+echo "Installing MATIO"
+./configure >> $LOG
+make >> $LOG
+sudo make install >> $LOG
 cd ..
 
 # GSL
 wget http://mirror.nexcess.net/gnu/gsl/gsl-2.1.tar.gz
 tar -xvzf gsl-2.1.tar.gz
 cd gsl-2.1
-./configure && make && sudo make install
+echo "Installing GSL"
+./configure >> $LOG
+make >> $LOG
+sudo make install >> $LOG
 cd ..
 
 rm *.tar.*

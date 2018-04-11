b4385e3fc1043403593e427d4acf58316f3da147
Andrew Cox <andrewdcox10@gmail.com>
2018-04-11 11:19:46 -0400

    temp3 to modify LTPOs for Robert

--- tests/temp3.cpp
+++ tests/temp3.cpp
@@ -7,6 +7,19 @@
 
 using namespace astrohelion;
 
+/**
+ * @brief Free the memory containing the loaded control laws
+ * 
+ * @param laws reference to a vector of control law pointers that are stored
+ * on the stack
+ */
+void freeLaws(std::vector<ControlLaw*>& laws){
+	for(auto L : laws){
+		delete L;
+		L = nullptr;
+	}
+}//====================================================
+
 int main(int argc, char** argv){
 	(void) arc;
 	(void) argv;
@@ -20,6 +33,31 @@ int main(int argc, char** argv){
 	std::vector<ControlLaw*> laws {};
 	fam.readFromMat(filename, laws);
 
-	
+	std::vector<double> yL4 {0.5*sqrt(3)};
+	Event evt_yL4 = Event(Event_tp::XZ_PLANE, 0, true, yL4);
+
+	int oID = 0;
+	double tof = 0;
+	std::vector<double> q0 {}, ctrl0 {};
+	for(unsigned int m = 0; m < fam.getNumMembers(); m++){
+		Arcset_Periodic mem = fam.getMember(m);
+		mem.putInChronoOrder();
+
+		for(unsigned int s = 0; s < mem.getNumSegs(s); s++){
+			const Segment seg& = mem.getSegRefByIx_const(s);
+			oID = seg.getOrigin();
+			if(oID == Linkable::INVALID_ID){
+				freeLaws(laws);
+				throw Exception("Origin ID is INVALID_ID?!");
+			}
+
+			q0 = mem.getState(oID);
+			if(seg.getCtrlLaw()){
+				ctrl0 = mem.getNodeRef_const(oID).getExtraParamVec(PARAMKEY_CTRL);
+			}
+
+			
+		}
+	}
 	return 0;
 }

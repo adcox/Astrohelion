e3edc7be6461eb4fda0b1d738500b03ec05aafdd
Andrew Cox <andrewdcox10@gmail.com>
2018-05-17 16:55:27 -0700

    began writing test for rot vs inert thrust in temp_astroTest

--- tests/temp_astroTest.cpp
+++ tests/temp_astroTest.cpp
@@ -9,19 +9,63 @@ using namespace astrohelion;
 using ltlaw = astrohelion::ControlLaw_cr3bp_lt;
 
 int main(int argc, char** argv){
-	// Check the A-matrix for the CR3BP-LT with GEN_INERT
+	
+	(void) argc;
+	(void) argv;
+
 	SysData_cr3bp_lt sys("earth", "moon", 100);
+	double theta0 = 0.123, f = 1e-2, Isp = 1500;
+	double t0 = -0.1, tof = PI;
+	unsigned int numSegs = 2;
+
+	double seg_tof = tof/static_cast<double>(numSegs);
+
+	// Law for inertially-fixed thrust vector
 	unsigned int lawID = ltlaw::GEN_INERT | ltlaw::CONST_F | ltlaw::CSI_VAR_M;
-	std::vector<double> params {-3.134, 1e-2, 1500};
-	ControlLaw_cr3bp_lt law(lawID, params);
-	EOM_ParamStruct eomParams(&sys, &law);
+	std::vector<double> params {theta0, f, Isp};
+	ControlLaw_cr3bp_lt inertLaw(lawID, params);
 
-	// q = {x, y, z, vx, vy, vz, m, psi, beta}
-	double q0[] = {0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0, 0.123, 0.321};
-	double t = 10.4;
+	// Law for rotating-frame-fixed thrust vector
+	lawID = ltlaw::GENERAL | ltlaw::CONST_F | ltlaw::CSI_VAR_M;
+	params = std::vector<double> {f, Isp};
+	ControlLaw_cr3bp_lt rotLaw(lawID, params);
+	
+	double a_desired = 0.4;
 
-	sys.getDynamicsModel()->finiteDiff_checkAMat(q0, t, 1e-7, &eomParams, 
-		Verbosity_tp::ALL_MSG);
+	std::vector<double> q0 {-0.6281, 0, 0, 0, -0.8723, 0, 1};
+	std::vector<double> rotCtrl0 {a_desired, 0};
+
+	SimEngine sim;
+	sim.setVerbosity(Verbosity_tp::NO_MSG);
+
+	Arcset_cr3bp_lt arc_rotFixed(&sys), arc_inertFixed(&sys);
+
+	// Single propagation for arc with thrust fixed in rotating frame
+	sim.runSim(q0, rotCtrl0, t0, tof, &arc_rotFixed, &rotLaw);
+
+	Arcset_cr3bp_lt temp(&sys);
+	std::vector<double> qi = q0;
+	for(unsigned int s = 0; s < numSegs; s++){
+		std::vector<double> inertCtrl0 {a_desired + theta0 + 
+			t0 + s*seg_tof + 0.5*seg_tof, 0};
+
+		temp.reset();
+		sim.runSim(qi, inertCtrl0, t0 + s*seg_tof, seg_tof, &temp, &inertLaw);
+		qi = temp.getStateByIx(-1);
+		if(s == 0){
+			arc_inertFixed = temp;
+		}else{
+			arc_inertFixed.appendSetAtNode(&temp, 
+				arc_inertFixed.getNodeRefByIx(-1).getID(),
+				temp.getNodeRefByIx(0).getID(), 0);
+		}
+	}
+
+	arc_inertFixed.putInChronoOrder();
+	arc_inertFixed.print();
+
+	arc_rotFixed.saveToMat("arc_rotFixed.mat");
+	arc_inertFixed.saveToMat("arc_inertFixed.mat");
 
 	return EXIT_SUCCESS;
 }
